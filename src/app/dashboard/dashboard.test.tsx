11a4a370d563dc925b56e534dae77cce8a9d64f9
Darlene Harsana <dharsana@ucsc.edu>
2025-06-23 14:53:35 +0200

    tests for dashboardto be wrapped in flex

--- src/app/dashboard/dashboard.test.tsx
+++ src/app/dashboard/dashboard.test.tsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Page from "@/app/dashboard/page";
 
+jest.mock("@/lib/prisma");
+
+describe('dashboard', () => {
+    it('should be wrapped in flex', async () =>{
+        const element = await Page();
+        
+        render(element);
+
+        const grid = screen.getByTestId("project-grid");
+        expect(grid).toHaveClass("flex");
+    })
+})
